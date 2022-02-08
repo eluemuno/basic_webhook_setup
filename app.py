3dde2796e2c6dd3d1397b87553356cbf2aff5bdc
Eluem-uno <75106535+eluemuno@users.noreply.github.com>
2022-02-08 17:05:09 -0500

    Updated entries

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-from flask import Flask, request, json
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Sherpa webhooks test'
+    return 'Simple Webhooks Test'
 
-# Sherpa webhookID--635c5ee0-51f8-11ec-b2d3-3bfc26f017e4
-@app.route('/sherpaHooks', methods=['POST'])
-def sherpaHook():
+@app.route('/testHooks', methods=['POST'])
+def testHook():
     data = request.json
     return data
-
-@app.route('/testhook', methods=['POST'])
-def testWebHooks():
-    return "Webhooks test confirmed, Nnaemeka"
 
 if __name__ == '__main__':
     app.run(debug=True, port=1980)
